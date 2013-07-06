99fa8d73f7f77a0e30ebb5fd7889f5ca47a3ba3f
irth <irth03@gmail.com>
2013-07-06 12:11:29 -0700

    go email

--- samples/scrumptious/routes/home.js
+++ samples/scrumptious/routes/home.js
@@ -16,25 +16,9 @@ exports.index = function(req, res) {
         res.render('index', {
             title: 'Nanogamers',
             loginUrl: FB.getLoginUrl({ scope: 'user_about_me, email' })
-        }); 
+        });
     } else {
         res.render('menu');
-        (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/all.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-         window.fbAsyncInit = function() {
-    // init the FB JS SDK
-    FB.init({
-      appId      : 'YOUR_APP_ID',                        // App ID from the app dashboard
-      channelUrl : '//WWW.YOUR_DOMAIN.COM/channel.html', // Channel file for x-domain comms
-      status     : true,                                 // Check Facebook Login status
-      xfbml      : true                                  // Look for social plugins on the page
-    }); 
-});
         updateUserInfo();
     }
 };
